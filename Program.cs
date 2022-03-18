c32f8a48240ab6a9f1723ad96b3f6ad9ef3ae614
zzwx <8169082+zzwx@users.noreply.github.com>
2022-03-18 16:46:52 -0400

    Singleton to run just one instance

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
 namespace DisplayedAppSwitcher {
   internal static class Program {
+    static Mutex mutex = new Mutex(true, "{E6DA3870-D05C-49A7-874D-0A3E6A121F38}");
+
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
     static void Main() {
-      // To customize application configuration such as set high DPI settings or default font,
-      // see https://aka.ms/applicationconfiguration.
-
-      ApplicationConfiguration.Initialize();
-      var mainForm = new SettingsForm();
-
-      var applicationContext = new ApplicationContext(mainForm);
-      Application.Run(applicationContext);
-
+      if (mutex.WaitOne(TimeSpan.Zero, true)) {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        var mainForm = new SettingsForm();
+        var applicationContext = new ApplicationContext(mainForm);
+        Application.Run(applicationContext);
+        mutex.ReleaseMutex();
+      }
     }
   }
 }
